5ebdb32e454ed57a033695fd3d781dceb4dc26d7
Daniel Parfionov <daniel.parfionov@gmail.com>
2017-01-09 18:22:34 +0200

    hover

--- craft.php
+++ craft.php
@@ -1,10 +1,13 @@
 <?php include 'header.php'; ?>
 
+<script src="js/script.js"></script>
+
+
       <div class="row">
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/beer-contents.gif">
+              <img class="gif" src="images/beer-contents.gif">
             </div>
             <div class="card-content">
               <p>Tikras alus susideda tik iš keturių ingridientų: vandens, salyklo, apynių bei mielių. Pagal ES standartus jei į alų yra įdedami papildomi ingridientai jo pavadinimas pasikeičia iš "Alaus" į "Specialios technologijos alų".</p>
@@ -14,7 +17,7 @@
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/beer-milling.gif">
+              <img class="gif" src="images/beer-milling.gif">
             </div>
             <div class="card-content">
               <p>Pirmas alaus gamybos etapas yra salyklo paruošimas - jis skrudinamas, kad išgauti sodresnę alaus spalvą. Vėliau sakyklas traiškomas, kad būtų lengviau išgauti viduje esantį cukrų bei krakmolą.</p>
@@ -24,7 +27,7 @@
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/beer-mashing.gif">
+              <img class="gif" src="images/beer-mashing.gif">
             </div>
             <div class="card-content">
               <p>Antrame etape salyklas yra salinamas. Tuo metu vyksta salyklo grūduose esančio netirpaus vandenyje krakmolo apcukrinimas. Netirpus krakmolas tampa tirpiu cukrų ir dekstrinų mišiniu.</p>
@@ -34,7 +37,7 @@
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/beer-boiling.gif">
+              <img class="gif" src="images/beer-boiling.gif">
             </div>
             <div class="card-content">
               <p>Trečiame etape salyklas buna atskiriamas nuo skysčio. Gautas skystys yra vadinamas misa. Ji verdama pagal specialų receptą įdedant kartinimo bei aromatizacijos apynius tam tikrais laiko intervalais.</p>
@@ -44,7 +47,7 @@
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/beer-chilling.gif">
+              <img class="gif" src="images/beer-chilling.gif">
             </div>
             <div class="card-content">
               <p>Vėliau misa turi būti kuo greičiau atšaldoma. Alaus pramonėje dažniausiai yra naudojami dideli šilumokaičiai, kurie sugeba 2000 litrų misos atšaldyti iki 20 laipsnių Celsijaus per vieną valandą.</p>
@@ -54,7 +57,7 @@
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/beer-fermenting.gif">
+              <img class="gif" src="images/beer-fermenting.gif">
             </div>
             <div class="card-content">
               <p>Penktame etape misa yra supilama į fermantacijos indus bei įdedamos mieles. Fermentacija dažniausiai trunka apei savaitę, kurios metu mielės maitinsis misoje esančiu cukrumi ir išskirs alkoholį.</p>
@@ -64,7 +67,7 @@
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/beer-bottling.gif">
+              <img class="gif" src="images/beer-bottling.gif">
             </div>
             <div class="card-content">
               <p>Paskutinis alaus gamybos etapas yra alaus išpilstymas į butelius. Išpilstytas alus dažniausiai brandinamas porą savaičių. Kai kurios alaus rūšys brandinamos net šešis mėnesius.</p>
@@ -74,7 +77,7 @@
         <div class="col s12 m6 l4">
           <div class="card hoverable">
             <div class="card-image">
-              <img src="images/daniel-beer.jpg">
+              <img class="gif" src="images/daniel-beer.jpg">
             </div>
             <div class="card-content">
               <p>Summer Ale - Citrusy Bitterness buvo vienas iš pirmųjų Daniel Parfionov išvirtų alų. Jis pasižymėjo švelniais vaisiniais ir esterių pustoniais bei citrusinių bei kitų tropinių vaisių skoniu.</p>
--- js/script.js
+++ js/script.js
@@ -2,3 +2,23 @@ $( document ).ready(function(){
 
   $(".button-collapse").sideNav();
 })
+
+$(document).ready(function()
+{
+    $(".gif").hover(
+        function()
+        {
+          var src = $(this).attr("src");
+          $(this).attr("src", src.replace(/\.png$/i, ".gif"));
+        },
+        function()
+        {
+          var src = $(this).attr("src");
+          $(this).attr("src", src.replace(/\.gif$/i, ".png"));
+        });
+});
+
+$(window).load(function(){
+	console.log("labas");
+	$('.gif').trigger("mouseover");
+});
